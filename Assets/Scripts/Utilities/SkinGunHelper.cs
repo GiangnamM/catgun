ef4abb1c5774c3a03cbca6c975fa7c9d5ba48c49
Nammai <132418954+MaigiangNam0@users.noreply.github.com>
2023-12-10 12:23:04 +0700

    Update skin gun helper

---
+++ Assets/Scripts/Utilities/SkinGunHelper.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace App
+{
+    public class SkinGunHelper
+    {
+        public static Dictionary<GunSkin, DefaultSkinGunManager.SkinConfig> SkinConfigs()
+        {
+            return new Dictionary<GunSkin, DefaultSkinGunManager.SkinConfig>()
+            {
+                {
+                    GunSkin.M4A1, new DefaultSkinGunManager.SkinConfig()
+                    {
+                        cost = 0,
+                    }
+                },    {
+                    GunSkin.Bazooka, new DefaultSkinGunManager.SkinConfig()
+                    {
+                        cost = 200,
+                    }
+                },    {
+                    GunSkin.FireBlaster, new DefaultSkinGunManager.SkinConfig()
+                    {
+                        cost = 400,
+                    }
+                },
+                {
+                    GunSkin.Laser, new DefaultSkinGunManager.SkinConfig()
+                    {
+                        cost = 800,
+                    }
+                },
+            };
+        }
+
+        public static List<GunSkin> AllSkins()
+        {
+            return new List<GunSkin>()
+            {
+                GunSkin.M4A1,
+                GunSkin.Bazooka,
+                GunSkin.FireBlaster,
+                GunSkin.Laser,
+            };
+        }
+    }
+}
